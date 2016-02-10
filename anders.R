8e2b4f25335b7a9326ee658a37da3dfe80ceeb41
anders1991 <anders.geil@gmail.com>
2016-02-10 15:55:31 +0100

    added metadata to corpus

--- anders.R
+++ anders.R
@@ -1,6 +1,28 @@
+library(magrittr)
 library(tm)
+
 
 # Load data
 src <- "~/Google Drev/tavila/documents"
 corpus <- Corpus(DirSource(src, encoding="latin1"), 
                  readerControl=list(Language="PlainTextDocument"))
+
+corpus[[3]] <- NULL # note: letter 100 = rapidminer-trash!
+
+
+# Metadata
+meta_src <- "~/Google Drev/tavila/metadata.csv"
+metadata <- read.csv(meta_src)
+
+metadata$dokument.nr <- as.character(metadata$dokument.nr) # tm's rækkefølge er efter chars
+metadata <- arrange(metadata, dokument.nr)
+
+for (j in 2:14) {
+    tags <- names(metadata)
+    for (i in 1:690) {
+        meta(corpus[[i]], tag=tags[j]) <- metadata[i,j]
+    }
+}
+
+
+
