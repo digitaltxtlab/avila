1ac6ffd9c05c2cb3994cfea7bab4747643782cdf
anders1991 <anders.geil@gmail.com>
2016-02-16 14:32:54 +0100

    Words written each year (plot)

--- anders.R
+++ anders.R
@@ -2,6 +2,7 @@ library(magrittr)
 library(tm)
 library(plyr)
 library(ggplot2)
+library(scales)
 library(leaflet)
 library(quanteda)
 
@@ -29,37 +30,58 @@ for (j in 2:14) {
 
 
 # Overview plots
-ggplot(metadata, aes(årstal)) +
+gg_year <- ggplot(metadata, aes(årstal)) +
     geom_histogram(binwidth=1) +
     xlab("") +
     ylab("Number of texts")
 
-ggplot(metadata, aes(as.factor(modtager))) +  # clean up modtager-list?
+gg_receiver <- ggplot(metadata, aes(as.factor(modtager))) +  # clean up modtager-list?
     geom_bar() +
     xlab("") +
     ylab("Number of texts") +
     ggtitle("Receiver") +
     coord_flip()
 
-ggplot(metadata, aes(as.factor(modtagerby))) +
+gg_receivercity <- ggplot(metadata, aes(as.factor(modtagerby))) +
     geom_bar() +
     xlab("") +
     ylab("Number of texts") +
     ggtitle("Receiver city") +
     coord_flip()
 
-ggplot(metadata, aes(as.factor(afsenderby))) +
+gg_sendercity <- ggplot(metadata, aes(as.factor(afsenderby))) +
     geom_bar() +
     xlab("") +
     ylab("Number of texts") +
     ggtitle("Sender city") +
     coord_flip()
 
-ggplot(metadata, aes(as.factor(metadata[,3]))) +
+gg_type <- ggplot(metadata, aes(as.factor(metadata[,3]))) +
     geom_bar() +
     xlab("") +
     ylab("Number of texts") +
     ggtitle("Type of text") +
     coord_flip()
 
-# Maps
+# Word-production per year
+words <- as.vector(NULL)
+for (i in 1:690) {
+    words[i] <- as.character(corpus[[i]][1]) %>% 
+        strsplit(., " ") %>% 
+        unlist() %>% 
+        length()
+}
+
+yearword <- cbind(year = metadata$årstal, words) %>% 
+    as.data.frame()
+yearword <- aggregate(yearword$words, list(yearword$year), sum)
+names(yearword) <- c("year", "words")
+yearword$year <- paste0(yearword$year, "-01-01") %>% 
+    as.Date(., "%Y-%m-%d")
+
+gg_yearword <- ggplot(yearword, aes(x=year, y=words)) +
+    geom_bar(stat="identity") +
+    xlab("") +
+    ylab("Words") +
+    scale_x_date(labels = date_format("%Y"),
+                 breaks = date_breaks("3 year"))
